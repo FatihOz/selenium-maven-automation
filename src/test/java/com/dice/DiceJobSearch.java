f0250cddcf734980ff0213c15c3d018be8f32bcc
OZBEK <fozbek@gmail.com>
2018-06-13 18:36:28 -0400

    print test and timestamp

--- src/test/java/com/dice/DiceJobSearch.java
+++ src/test/java/com/dice/DiceJobSearch.java
@@ -1,6 +1,7 @@
 
 package com.dice;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -60,7 +61,7 @@ public class DiceJobSearch {
 		}
 		
 		driver.close();
-				
+				System.out.println("TEST COMPLETED -" + LocalDateTime.now());
 	}
 }
 
